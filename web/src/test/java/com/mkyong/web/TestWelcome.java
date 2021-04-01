2290c16932db8ccb850558f56511f6ec56cdbe02
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2021-04-01 21:58:26 +0200

    Added target to project

--- web/src/test/java/com/mkyong/web/TestWelcome.java
+++ web/src/test/java/com/mkyong/web/TestWelcome.java
@@ -34,14 +34,6 @@ public class TestWelcome {
     @Test
     public void testDefault() throws Exception {
 
-        this.mockMvc.perform(
-                get("/"))
-                //.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"))
-                .andExpect(model().attribute("query", "123456"));
-        //.andExpect(model().attribute("sha256", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"))
-        //.andExpect(model().attribute("md5", "e10adc3949ba59abbe56e057f20f883e"));
 
     }
 
